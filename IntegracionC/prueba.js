db6b8079caec7b707a40ea79f146c27cefe83980
Gabucho 14 <gabarcebsc15@gmail.com>
2021-02-10 16:34:53 -0500

    Añadir testeo unitario

--- IntegracionC/prueba.js
+++ IntegracionC/prueba.js
@@ -1,6 +1,14 @@
 const express= require('express');
-const app = express();
+const routes = express();
 
-app.listen(3000, (req, res)=>{
+routes.get('/servidor',(req, res)=>{
+    res.send('Bienvendios a mi servidor');
+})
+routes.post('/aplicacion',(req, res)=>{
+    res.send('Bienvendios a mi aplicacion');
+})
+
+routes.listen(3000, (req, res)=>{
     console.log("HELLO WORLD.........")
 })
+module.exports = routes;
